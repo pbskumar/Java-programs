fa3059de713b9a00a1666c315f2e2dedb50e0b40
Brahmendra Sravan Kumar Patibandla <pbskumar@live.com>
2016-04-01 16:56:16 -0400

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -11,55 +11,67 @@
 
 import java.util.*;
 public class Permutations {
-    
-	public static long count = 0;
 	
-  public static void getPermutations(ArrayList<Integer> num_list) {
+	/**
+	 * getPermutations is set of overloaded funtions which use ArrayList data structure to generate all possible permutations.
+	 * The function with one input takes the input sequence and call the second function with two inputs.
+	 * THe function iterates through the ArrayList, selecting one element every iteration. 
+	 * In each iteration, the remaining elements are passed as num_list to getPermutations().
+	 * The selected element is added to the current_sequence and passed to the next recursion level.
+	 * The recursion ends when the num_list is empty and prints the sequence.
+	 */
+  	public static void getPermutations(ArrayList<Integer> num_list) {
+    	
+    	ArrayList<Integer> temp_num_list = null;
+    	ArrayList<Integer> rest_num = null;
+    	ArrayList<Integer> sequence = null;
+    	temp_num_list = new ArrayList<Integer>(num_list);
     
-    ArrayList<Integer> temp_num_list = null;
-    ArrayList<Integer> rest_num = null;
-    ArrayList<Integer> sequence = null;
+		for(int i = 0; i < temp_num_list.size(); i++) {
+      		rest_num = new ArrayList<Integer>(temp_num_list);
+	      	rest_num.remove(i);
+	      	sequence = new ArrayList<Integer>();
+	      	sequence.add(temp_num_list.get(i));
+	      	getPermutations(rest_num, sequence);
+		}    
+	}
     
-    temp_num_list = new ArrayList<Integer>(num_list);
+  	public static void getPermutations(ArrayList<Integer> num_list, ArrayList<Integer> current_sequence) {
+    	
+    	ArrayList<Integer> temp_num_list = null;
+    	ArrayList<Integer> temp_rest_num = null;
+    	ArrayList<Integer> temp_sequence = null;
     
-    for(int i = 0; i < temp_num_list.size(); i++) {
-      rest_num = new ArrayList<Integer>(temp_num_list);
-      rest_num.remove(i);
-      sequence = new ArrayList<Integer>();
-      sequence.add(temp_num_list.get(i));
-      getPermutations(rest_num, sequence);
-    }    
-  }
-    
-  public static void getPermutations(ArrayList<Integer> num_list, ArrayList<Integer> current_sequence) {
-    
-    ArrayList<Integer> temp_num_list = null;
-    ArrayList<Integer> temp_rest_num = null;
-    ArrayList<Integer> temp_sequence = null;
-    
-    if(num_list.size() == 0)  {
-      for(Integer n : current_sequence) {
-        System.out.print(n);
-      }
-      System.out.println();
-    }
-    else  {
-      temp_num_list = new ArrayList<Integer>(num_list);
+    	if(num_list.size() == 0)  {
+      		for(Integer n : current_sequence) {
+        		System.out.print(n);
+      		}
+      		System.out.println();
+    	}
+    	else  {
+      		temp_num_list = new ArrayList<Integer>(num_list);
         
-      for(int i = 0; i < temp_num_list.size(); i++) {
-        temp_rest_num = new ArrayList<Integer>(temp_num_list);
-        temp_rest_num.remove(i);
-        temp_sequence = new ArrayList<Integer>(current_sequence);
-        temp_sequence.add(temp_num_list.get(i));
-        getPermutations(temp_rest_num, temp_sequence);
-      }
-    }
-  }
+	      	for(int i = 0; i < temp_num_list.size(); i++) {
+		        temp_rest_num = new ArrayList<Integer>(temp_num_list);
+		        temp_rest_num.remove(i);
+		        temp_sequence = new ArrayList<Integer>(current_sequence);
+		        temp_sequence.add(temp_num_list.get(i));
+		        getPermutations(temp_rest_num, temp_sequence);
+			}
+    	}
+  	}
 
+	/**
+	 * permutations is set of overloaded funtions which use String as input to generate all possible permutations.
+	 * The function with one input takes the input sequence and call the second function with two inputs.
+	 * THe function iterates through the each character of str, selecting one element every iteration. 
+	 * In each iteration, the remaining elements are passed as rest_str to permutations().
+	 * The selected element is added to the current_sequence and passed to the next recursion level.
+	 * The recursion ends when the rest_str is empty and prints the sequence.
+	 */
 	public static void permutations(String str) {
 		permutations(str, "");
 	}
-	
 	public static void permutations(String rest_str, String current_sequence) {
 		if(rest_str.length() == 0)  {
 			System.out.println(current_sequence);
@@ -71,13 +83,13 @@ public class Permutations {
 		}
 	}
 	
-  public static void main(String[] args)  {
-    Integer[] num1 = new Integer[] {0, 1, 2, 3};	
+  	public static void main(String[] args)  {
+  		Integer[] num1 = new Integer[] {0, 1, 2, 3};	
 		String str = "0123";		
 		ArrayList<Integer> num_list = new ArrayList<>(Arrays.asList(num1));    
-		
+	
 		permutations(str);
 		System.out.println("\n**********************************************\n");
 		getPermutations(num_list);
-  }
+  	}
 }
